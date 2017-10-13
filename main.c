1a8670818f65ff8bda36b486d1847662c4dc09b1
Jacopo Nasi <jacopo.nasi@gmail.com>
2017-10-13 11:27:13 +0200

    Adding main.c with Copyright and .gitignore for CLion

---
+++ main.c
@@ -0,0 +1,11 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *          OMA Project of Jacopo Maggio, Stefano Munna, Jacopo Nasi, Andrea Santu and Marco Torlaschi               *
+ *                      Repository available on https://github.com/Jacopx/OMA_ExamTimeTable                          *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
