29862972609644b53d9d7b8134c8fa6524cd4cfa
torla <marcotorlaschi95@gmail.com>
2017-12-27 17:48:54 +0100

    fix

--- local.c
+++ local.c
@@ -9,44 +9,10 @@
 #include "utils.h"
 #include <time.h>
 
-const float minimiumDelta = 0.00001;
+const float minimiumDelta = 0.000001;
 
 void localSearch(dataStructure *sol, int maxTime) {
-    int e,s,startTime;
-    float delta;
-    struct {
-        int exam;
-        int slot;
-        float delta;
-    }best;
-    startTime=time(NULL);
-    while(time(NULL)-startTime<maxTime){
-        best.exam=-1;
-
-        best.slot=-1;
-        best.delta=100000000000;
-        for(e=0;e<sol->E;e++){
-            for(s=1;s<=sol->timeSlots;s++){
-                if(isFeasibleThis(sol,sol->exams,e,s)){
-                    delta=benchmarkSolutionDeltaMove(sol,sol->exams,e,sol->exams[e],s);
-                    if(delta<best.delta){
-                        best.exam=e;
-                        best.slot=s;
-                        best.delta=delta;
-                    }
-                }
-            }
-        }
-        if(best.delta<-minimiumDelta){
-            sol->exams[best.exam]=best.slot;
-#ifdef VERBOSE_LOCAL
-            printf("\n%d->%d (%.3f,%.3f)",best.exam,best.slot,best.delta,benchmarkSolution(sol,sol->exams));
-#endif
-        }
-        else break;
-    }
-	localSearch2Temp(sol,sol->exams,maxTime-(time(NULL)-startTime));
-	localSearchSlideTemp(sol,sol->exams,maxTime-(time(NULL)-startTime));
+	localSearchTemp(sol,sol->exams,maxTime);
 }
 void localSearchTemp(dataStructure *sol,int* temp, int maxTime) {
     int e,s,startTime;
@@ -85,7 +51,7 @@ void localSearchTemp(dataStructure *sol,int* temp, int maxTime) {
 	localSearch2Temp(sol,temp,maxTime-(time(NULL)-startTime));
 	localSearchSlideTemp(sol,temp,maxTime-(time(NULL)-startTime));
 	if(old-benchmarkSolution(sol,temp)>minimiumDelta){
-		localSearchTemp(sol,temp,time(NULL)-startTime);
+		localSearchTemp(sol,temp,maxTime-(time(NULL)-startTime));
 	}
 }
 void swapExam(int *sol,int a,int b){
